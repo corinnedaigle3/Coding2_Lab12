60f3e4bde7dd5543fdea63f0cbda3b2bd7d6798e
corinnedaigle3 <corinnedaigle9@gmail.com>
2025-11-05 13:44:20 -0500

    script setup

---
+++ Assets/Script/InventoryItem.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryItem : MonoBehaviour
+{
+    public string id;
+    public string name;
+    public int value;
+
+    List<string> inventoryItems = new List<string>();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/InventoryManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Initialization code here
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
